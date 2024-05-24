83ddd667acfe0380383750eb68919f0170d35619
loop0919 <ruupu2001@gmail.com>
2024-05-24 10:53:01 +0900

    [Fix] 別タブでGetメソッドをロードしたときにbook repositoryの内容が出ない不具合を修正

--- src/main/java/jp/te4a/spring/boot/my_boot_app6/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/my_boot_app6/HelloController.java
@@ -2,7 +2,6 @@ package jp.te4a.spring.boot.my_boot_app6;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,20 +14,20 @@ public class HelloController {
     BookService bookService;
 
     @GetMapping("books/list")
-    public String index(Model model) {
-        model.addAttribute("msg", "this is a setting message");
-        return "books/list";
+    public ModelAndView index(ModelAndView mv) {
+        mv.addObject("books", bookService.findAll());
+        return mv;
     }
     
     @PostMapping("books/list")
-    public ModelAndView postMethodName(
+    public ModelAndView postBookData(
         @RequestParam("id") String id,
         @RequestParam("title") String title,
         @RequestParam("writer") String writer,
         @RequestParam("publisher") String publisher,
-        @RequestParam("price") String price
+        @RequestParam("price") String price,
+        ModelAndView mv
     ) {
-        var mv = new ModelAndView("books/list");
         var bookBean = new BookBean(
             Integer.valueOf(id), 
             title, 
